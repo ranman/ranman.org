631285ee4f8207a52a1943cd1728210eecca8ec8
J. Randall Hunt <randallhunt@gmail.com>
2014-03-25 05:55:25 -0400

    read events from database

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 from flask.ext.pymongo import PyMongo
+from datetime import datetime
 
-app = Flask(__name__)
+app = Flask("randall")
+app.debug = True
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def home_page():
-    return render_template('ranman.html')
+    events = list(mongo.db.events.find().sort("date", -1))
+    for index in range(len(events)):
+        if events[index].get('date').replace(tzinfo=None) <= datetime.now():
+            break
+    upcoming = events[:index]
+    past = events[index:]
+    return render_template('ranman.html', upcoming=upcoming, past=past)
 
 
 if __name__ == '__main__':
--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,33 @@
 window.onload = function() {
-    var projects = document.getElementById('projects-list');
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.github.com/search/repositories?q=user:ranman&sort=updated&per_page=100', true);
-    xhr.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function(e) {
-        var data = xhr.response.items;
-        var container = document.createDocumentFragment();
-        var listItem;
-        var proj;
-        for(var i=0; i<data.length; i++) {
-            proj = data[i];
-            listItem = document.createElement('li');
-            listItem.innerHTML = [
-                '<a href="',
-                proj.html_url,
-                '" title="',
-                proj.description,
-                '">',
-                proj.name,
-                '</a> - ',
-                (proj.description || '')
-            ].join('')
-            container.appendChild(listItem);
-        }
-        projects.appendChild(container);
-    }, false);
-    xhr.send();
+	var projects = document.getElementById('projects-list');
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://api.github.com/search/repositories?q=user:ranman&sort=updated&per_page=100', true);
+	xhr.setRequestHeader('Accept', 'application/vnd.github.v3+json');
+	xhr.responseType = 'json';
+	xhr.addEventListener('load', function(e) {
+		var data = xhr.response.items;
+		var container = document.createDocumentFragment();
+		var listItem;
+		var proj;
+		for(var i=0; i<data.length; i++) {
+			proj = data[i];
+			listItem = document.createElement('li');
+			listItem.innerHTML = [
+				'<a href="',
+				proj.html_url,
+				'" title="',
+				proj.description,
+				'">',
+				proj.name,
+				'</a> - ',
+				(proj.description || '')
+			].join('');
+			if (i > 9) {
+				listItem.style.display = "none";
+			}
+			container.appendChild(listItem);
+		}
+		projects.appendChild(container);
+	}, false);
+	xhr.send();
 }
