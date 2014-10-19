bda93043b2ffdd5c3c24cbc7f585769dfa006a7b
J. Randall Hunt <randallhunt@gmail.com>
2014-10-19 05:56:57 -0400

    everything should be working now

--- server.py
+++ server.py
@@ -1,11 +1,73 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
+from flask_oauth import OAuth
+import os
+import json
+import requests
+import facebook
+from boto.sts import STSConnection
 from flask.ext.pymongo import PyMongo
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET")
 app.config.update({'MONGO_DBNAME': 'randall'})
-app.debug = True
 mongo = PyMongo(app)
+FACEBOOK_APP_ID = '776036972455787'
+FACEBOOK_APP_SECRET = os.getenv('FACEBOOK_APP_SECRET')
+oauth = OAuth()
+facebook_oauth = oauth.remote_app(
+    'facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ['email', 'user_groups']}
+)
+
+
+@app.route('/login')
+def login():
+    return facebook_oauth.authorize(
+        callback=url_for('oauth_authorized', _external=True))
+
+
+@facebook_oauth.tokengetter
+def get_facebook_token(token=None):
+    return session.get('facebook_token')
+
+
+@app.route('/oauth-authorized')
+@facebook_oauth.authorized_handler
+def oauth_authorized(resp):
+    facebook_graph = facebook.GraphAPI(resp.get('access_token'))
+    email = facebook_graph.get_object("me")['email']
+    sts_connection = STSConnection()
+    role = sts_connection.assume_role(
+        role_arn="arn:aws:iam::054060359478:role/facebook-login",
+        role_session_name=email
+    )
+    creds = {
+        'sessionId': role.credentials.access_key,
+        'sessionKey': role.credentials.secret_key,
+        'sessionToken': role.credentials.session_token
+    }
+    params = {
+        'Action': 'getSigninToken',
+        'Session': json.dumps(creds),
+    }
+    auth_url = "https://signin.aws.amazon.com/federation"
+    r = requests.get(auth_url, params=params)
+    signin_token = json.loads(r.text).get('SigninToken')
+    params.update({
+        'Action': 'login',
+        'Issuer': 'ranman.org',
+        'Destination': 'https://console.aws.amazon.com/',
+        'SigninToken': signin_token
+    })
+    url = requests.Request('GET', auth_url, params=params).prepare().url
+    return redirect(url)
 
 
 @app.route('/')
